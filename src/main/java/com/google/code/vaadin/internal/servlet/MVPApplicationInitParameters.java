ecb7e48746f3ac2c96d339c51517ee64668b9fbd
Aleksey.Krylov <Aleksey.Krylov@gmail.com>
2013-01-25 10:01:44 +0000

    [!] milestone

---
+++ src/main/java/com/google/code/vaadin/internal/servlet/MVPApplicationInitParameters.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2013, i-Free. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+
+package com.google.code.vaadin.internal.servlet;
+
+/**
+ * MVPApplicationInitParameters - TODO: description
+ *
+ * @author Alexey Krylov (lexx)
+ * @since 25.01.13
+ */
+public interface MVPApplicationInitParameters {
+
+    /*===========================================[ INTERFACE METHODS ]==============*/
+
+    String P_APPLICATION = "application";
+    String P_APPLICATION_MODULE = "application-module";
+}
